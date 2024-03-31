99455387bfb110020cdae89f93f20dc4437824c8
danialzadrafiee <leylasarafrezai@gmail.com>
2024-03-31 22:47:01 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,21 +10,35 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       crypto: 'crypto-browserify',
+      buffer: 'buffer',
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'replace-buffer',
+          transform(code, id) {
+            if (id.includes('node_modules/buffer/index.js')) {
+              return code.replace(/global\.Buffer/g, 'Buffer');
+            }
+          },
+        },
+      ],
     },
   },
   optimizeDeps: {
+    include: ['buffer'],
     esbuildOptions: {
       define: {
         global: 'globalThis'
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
-          buffer: true,
           crypto: true,
+          buffer: true,
         })
       ]
     }
   }
 });
-
-
